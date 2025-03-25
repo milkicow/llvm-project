be7aee1a0af48965b63a4f2d6dff2f485582aa0e
milkicow <kozlov.aleksandr@phystech.edu>
2025-03-26 01:33:18 +0300

    [Arch52] Add Arch52TargetMachine::getObjFileLowering

--- llvm/lib/Target/Arch52/Arch52TargetMachine.cpp
+++ llvm/lib/Target/Arch52/Arch52TargetMachine.cpp
@@ -1,6 +1,7 @@
 #include "Arch52TargetMachine.h"
 #include "Arch52.h"
 #include "TargetInfo/Arch52TargetInfo.h"
+#include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/MC/TargetRegistry.h"
 #include <optional>
@@ -21,7 +22,8 @@ Arch52TargetMachine::Arch52TargetMachine(const Target &T, const Triple &TT,
                                          CodeGenOptLevel OL, bool JIT)
     : LLVMTargetMachine(T, "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32", TT,
                         CPU, FS, Options, Reloc::Static,
-                        getEffectiveCodeModel(CM, CodeModel::Small), OL) {
+                        getEffectiveCodeModel(CM, CodeModel::Small), OL),
+      TLOF(std::make_unique<TargetLoweringObjectFileELF>()) {
   ARCH52_DUMP_CYAN
   initAsmInfo();
 }
@@ -50,4 +52,9 @@ public:
 TargetPassConfig *Arch52TargetMachine::createPassConfig(PassManagerBase &PM) {
   ARCH52_DUMP_CYAN
   return new Arch52PassConfig(*this, PM);
+}
+
+TargetLoweringObjectFile *Arch52TargetMachine::getObjFileLowering() const {
+  ARCH52_DUMP_CYAN
+  return TLOF.get();
 }
--- llvm/lib/Target/Arch52/Arch52TargetMachine.h
+++ llvm/lib/Target/Arch52/Arch52TargetMachine.h
@@ -8,6 +8,8 @@ namespace llvm {
 extern Target TheArch52Target;
 
 class Arch52TargetMachine : public LLVMTargetMachine {
+  std::unique_ptr<TargetLoweringObjectFile> TLOF;
+
 public:
   Arch52TargetMachine(const Target &T, const Triple &TT, StringRef CPU,
                       StringRef FS, const TargetOptions &Options,
@@ -17,6 +19,7 @@ public:
 
   // Pass Pipeline Configuration
   TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+  TargetLoweringObjectFile *getObjFileLowering() const override;
 };
 } // end namespace llvm
 
