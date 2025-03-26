6145d207936d315149da893241bd5a38191e230e
milkicow <kozlov.aleksandr@phystech.edu>
2025-03-26 13:22:28 +0300

    [Arch52] Add full Arch52InstPrinter

---
+++ llvm/lib/Target/Arch52/MCTargetDesc/Arch52InstPrinter.cpp
@@ -0,0 +1,45 @@
+#include "Arch52InstPrinter.h"
+#include "MCTargetDesc/Arch52Info.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "asm-printer"
+
+#include "Arch52GenAsmWriter.inc"
+
+void Arch52InstPrinter::printRegName(raw_ostream &O, MCRegister Reg) const {
+  O << getRegisterName(Reg);
+}
+
+void Arch52InstPrinter::printInst(const MCInst *MI, uint64_t Address,
+                               StringRef Annot, const MCSubtargetInfo &STI,
+                               raw_ostream &O) {
+  printInstruction(MI, Address, O);
+  printAnnotation(O, Annot);
+}
+
+void Arch52InstPrinter::printOperand(const MCInst *MI, int OpNo, raw_ostream &O) {
+  const MCOperand &MO = MI->getOperand(OpNo);
+
+  if (MO.isReg()) {
+    printRegName(O, MO.getReg());
+    return;
+  }
+
+  if (MO.isImm()) {
+    O << MO.getImm();
+    return;
+  }
+
+  assert(MO.isExpr() && "Unknown operand kind in printOperand");
+  MO.getExpr()->print(O, &MAI);
+}
--- llvm/lib/Target/Arch52/MCTargetDesc/Arch52InstPrinter.h
+++ llvm/lib/Target/Arch52/MCTargetDesc/Arch52InstPrinter.h
@@ -2,7 +2,6 @@
 #define LLVM_LIB_TARGET_ARCH52_INSTPRINTER_ARCH52INSTPRINTER_H
 
 #include "llvm/MC/MCInstPrinter.h"
-#include "llvm/MC/MCRegister.h"
 
 namespace llvm {
 
@@ -12,14 +11,19 @@ public:
                     const MCRegisterInfo &MRI)
       : MCInstPrinter(MAI, MII, MRI) {}
 
-  std::pair<const char *, uint64_t> getMnemonic(const MCInst *MI) override {
-    return std::pair<const char *, uint64_t>{};
-  }
+  std::pair<const char *, uint64_t> getMnemonic(const MCInst *MI) override;
+  void printInstruction(const MCInst *MI, uint64_t Address, raw_ostream &O);
+  static const char *getRegisterName(MCRegister Reg);
+
+  // Print given operand
+  static void printOperand(const MCOperand &MO, raw_ostream &O);
 
   // Override MCInstPrinter.
-  void printRegName(raw_ostream &O, MCRegister Reg) const override {}
+  void printRegName(raw_ostream &O, MCRegister Reg) const override;
   void printInst(const MCInst *MI, uint64_t Address, StringRef Annot,
-                 const MCSubtargetInfo &STI, raw_ostream &O) override {}
+                 const MCSubtargetInfo &STI, raw_ostream &O) override;
+
+  void printOperand(const MCInst *MI, int OpNo, raw_ostream &O);
 };
 } // namespace llvm
 
